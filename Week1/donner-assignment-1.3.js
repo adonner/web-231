abfb1c96083821647ce213d5a1d56dbc5fe05871
Adam Donner <adam@adamdonner.com>
2019-02-07 16:42:20 -0600

    Adding Assignemt 1.3 and 1.4

---
+++ Week1/donner-assignment-1.3.js
@@ -0,0 +1,24 @@
+
+/*
+============================================
+; Title: donner-assignment-1.3.js
+; Author: Adam Donner
+; Date: 7 February 2019
+; Description: This program says Hello World
+; and introduces myself.
+;===========================================
+*/ 
+
+// start program
+
+/*
+    Expected Output:
+
+    Hello World, my name is Adam Donner
+*/
+
+
+// output
+console.log("Hello World, my name is Adam Donner")
+
+// end program
---
+++ Week1/donner-assignment-1.4.js
@@ -0,0 +1,43 @@
+/*
+============================================
+; Title: donner-assignment-1.4.js
+; Author: Adam Donner
+; Date: 7 February 2019
+; Description: This program demonstrates the
+; use of JavaScript values and variables 
+; using the variable deceleration and 
+; console.log
+;===========================================
+*/ 
+
+// start program
+
+/*
+    Expected Output:
+
+    First Name
+    Last Name
+    Age
+    Street Address
+    City
+    Zipcode
+*/
+
+// Variable declaration for contact information:
+var firstName = "Adam";
+var lastName = "Donner";
+var age = "37"
+var street = "14445 Bennington Way"
+var city = "Omaha"
+var zipCode = "68123"
+
+
+// output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
+// end program
