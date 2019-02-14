4752e4136629486cb9926be3d3e220f9573a6854
Adam Donner <adam@adamdonner.com>
2019-02-13 22:29:40 -0600

    week-2

---
+++ week-2/donner-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  donner-header-test.js
+; Author: Adam
+; Date:   13 February 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../donner-header.js');
+
+console.log(header.display("Adam", "Donner", "Week 2"));
