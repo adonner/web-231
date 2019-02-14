483474dc9170b1de3482777ac3b0fa79df39611a
Adam Donner <adonner@my365.bellevue.edu>
2019-02-14 11:46:35 -0600

    updated header

--- week-2/donner-header-test.js
+++ week-2/donner-header-test.js
@@ -1,12 +1,28 @@
 /*
-============================================
+=============================================
 ; Title:  donner-header-test.js
 ; Author: Adam
-; Date:   13 February 2019
+; Date:   14 February 2019
 ; Description: Displays a formatted header
-;===========================================
+;============================================
 */
 
+
+// start program
+
+/*
+    Expected Output:
+  Name
+  Assignment Week
+  Todays Date
+*/
+
+// Calls function to display required information
 const header = require('../donner-header.js');
 
+
+// output
 console.log(header.display("Adam", "Donner", "Week 2"));
+
+
+// end program
