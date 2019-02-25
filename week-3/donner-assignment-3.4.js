740be04dfc323a8a5677d4177f767ef9f87b99b7
Adam Donner <adonner@my365.bellevue.edu>
2019-02-24 19:30:29 -0600

    Updated function order

--- week-3/donner-assignment-3.4.js
+++ week-3/donner-assignment-3.4.js
@@ -5,8 +5,8 @@
 ; Date:   22 February 2019
 ; Description: Creates a for loop to check
 ; the value of a static number and a random
-; then display the results.  The for loop will
-; run for 10 iterations.
+; number then display the results.  The for
+; loop will run for 10 iterations.
 ;============================================
 */
 
@@ -19,7 +19,7 @@ Expected output:
   Assignment 3.4
   Todays Date
 
-  -- DO THE NUMBERS MATCH GAME --  // All chance numbers will be random, this is an example of the output for the match or mismatch.
+  -- DO THE NUMBERS MATCH GAME --
   6 does not match 4!
   6 does not match 7!
   6 does not match 4!
@@ -43,9 +43,9 @@ console.log("\n--DO THE NUMBERS MATCH GAME--")  // Writes the name of this game 
 // This declares variable and assigns it a value of 6.
 var checkNumber = 6;
 
-// This function defines an output for a mismatch of two two variables and writes a string and the passed in variables to the console.log.
-function logMismatch(checkNumber, chance) {
-  console.log(checkNumber + " and " + chance + " do not match!");
+// This function returns a random integer value.
+function randomNumber() {
+  return Math.floor((Math.random() * 10) + 1)
 }
 
 // This function defines an output for a match of two two variables and writes a string and the passed in variables to the console.log.
@@ -53,9 +53,9 @@ function logMatch(checkNumber, chance) {
   console.log(checkNumber + " and " + chance + " do match!")
 }
 
-// This function returns a random integer value.
-function randomNumber() {
-  return Math.floor((Math.random() * 10) + 1)
+// This function defines an output for a mismatch of two two variables and writes a string and the passed in variables to the console.log.
+function logMismatch(checkNumber, chance) {
+  console.log(checkNumber + " and " + chance + " do not match!");
 }
 
 // This for loop which declares a variable counter and assigns it a value of 0 to ensure the for loop will run for 10 iterations.
